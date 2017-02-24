ee723aacd66603043b76256edda605defff24073
lixindi <lixindi@baidu.com>
2017-02-24 16:40:39 +0800

    login page // todo: connect the db

---
+++ src/main/webapp/login/js/login.js
@@ -0,0 +1,17 @@
+/**
+ * Created by lixindi on 2017/2/20.
+ */
+var login = angular.module('login', []);
+login.controller('login_ctrl', function ($scope, $http) {
+    $scope.validate = function () {
+        $http.post('/validate', {username: $scope.username, password: $scope.password})
+            .success(function (response) {
+                if (response) {
+                    alert("登陆成功");
+                    window.location.href = "/admin/admin.html"    ;
+                } else {
+                    alert("登录失败");
+                }
+            })
+    }
+});
